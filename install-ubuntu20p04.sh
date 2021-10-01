a76775a265ca66f87386028c7a46a8baeeaadb1a
ssd-Ind <91599323+ssd-Ind@users.noreply.github.com>
2021-10-01 11:28:17 +0530

    Create install-ubuntu20p04.sh

---
+++ install-ubuntu20p04.sh
@@ -0,0 +1,7 @@
+sudo apt install ubuntu-desktop
+sudo apt install xrdp
+sudo systemctl status xrdp
+sudo adduser xrdp ssl-cert
+sudo ufw allow from 192.168.2.0/24 to any port 3389
+sudo ufw reload
+sudo ufw status
